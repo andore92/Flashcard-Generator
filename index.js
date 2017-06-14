49d8f726226b58319865e55b18e1b71eb4c2d4ab
Andrew Sadowski <Andrew Sadowski>
2017-06-14 13:05:28 -0400

    got working functions for playing the game, but need to make them dynamic

--- index.js
+++ index.js
@@ -3,17 +3,59 @@ var ClozeCard = require("./ClozeCard");
 var inquirer = require("inquirer");
 var cardData = require("./cardData.json");
 
+var basicCardsArr = cardData.cards.basic;
+var clozeCardsArr = cardData.cards.cloze;
 
 
-var basicCardArr = [];
-var clozeCardArr = [];
+
+
 
 var basicCount = 0;
 var clozeCount = 0;
 
+var basicCorrect = 0;
+var clozeCorrect = 0;
+
+function playBasicCards() {
+    
+      inquirer.prompt([
+        {
+          name: "question",
+          message: basicCardsArr[1].front
+        }
+        ]).then(function(answers){
+          if (answers.question === basicCardsArr[1].back) {
+            basicCorrect++;
+            console.log("correct");
+          } else {
+            console.log("incorrect");
+          }
+      })
+      
+       
+    }
+
+function playClozeCards() {
+    
+      inquirer.prompt([
+        {
+          name: "question",
+          message: clozeCardsArr[1].partial
+        }
+        ]).then(function(answers){
+          if (answers.question === clozeCardsArr[1].cloze) {
+            basicCorrect++;
+            console.log("correct");
+          } else {
+            console.log("incorrect");
+          }
+      })
+      
+       
+    }
 
 
-var createBasicCards = function () {
+function createBasicCards() {
   // if statement to ensure that our questions are only asked five times
   if (basicCount < 5) {
     console.log("Create new flashcard");
@@ -43,7 +85,7 @@ var createBasicCards = function () {
   }
 };
 
-var createClozeCards = function () {
+function createClozeCards() {
   // if statement to ensure that our questions are only asked five times
   if (clozeCount < 5) {
     console.log("Create new cloze flashcard");
@@ -73,7 +115,7 @@ var createClozeCards = function () {
   }
 };
 
-var startup = function () {
+function startup() {
   inquirer.prompt([
       {
         name: "startupQuestion",
@@ -81,20 +123,21 @@ var startup = function () {
       }
     ]).then(function(answers){
         var startupQuestion = answers.startupQuestion
-        if(startupQuestion === "no") {
+        if(startupQuestion === "yes") {
           inquirer.prompt([
         {
           name: "cardTypeSelection",
-          message: "Would you like to create Basic or Cloze cards?"
+          message: "Would you like to play with Basic or Cloze cards?"
         }
       ]).then(function(answers){
           var cardTypeSelection = answers.cardTypeSelection
           if(cardTypeSelection === "basic"){
-            createBasicCards();
-          } if(ClozeCard === "cloze" ) {
-            createClozeCards();
+            playBasicCards();
+          } if(cardTypeSelection === "cloze" ) {
+            playClozeCards();
           } 
       })
       }
     })
 }
+startup();
